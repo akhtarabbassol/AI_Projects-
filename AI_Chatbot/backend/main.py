de2ee80c783f467dea15179388839d8564dde9e3
ghayur-abbas <ghayur.abbas@kabirtechsolutions.com>
2026-08-21 17:40:45 +0500

    Update main.py
    commit change

--- AI_Chatbot/backend/main.py
+++ AI_Chatbot/backend/main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware  
 from pydantic import BaseModel,Field
 from dotenv import load_dotenv
-from typing import Optional
 from google import genai
 import os
 load_dotenv()
@@ -22,14 +21,41 @@ class ChatRequest(BaseModel):
     message: str = Field(..., description = "Ask Something! ")
 
 
+SYSTEM_INSTRUCTION ="""
+You are a professional AI assistant.
+
+Answer the user's question:
+- Clearly
+- Accurately
+- Concisely
+- In a professional and helpful manner
+
+Use bullet points or numbered lists when useful.
+For programming questions, provide clean and practical answers.
+Do not unnecessarily repeat the question.
+Do not invent information. If you are unsure, say so.
+"""
 @app.get('/')
 def home():
     return {"message":"Gemini Chatbot API is running: "}
 
 @app.post('/chat')
 def chat(request: ChatRequest):
-    response = client.models.generate_content(
-        model  = "gemini-2.5-flash",
-        contents = request.message
-    )
-    return {"response":response.text}
+    message = request.message.strip()
+    if not message:
+        raise HTTPException(status_code = 400, detail ="Message cannot be empty")
+    try:
+        response = client.models.generate_content(
+            model  = "gemini-2.5-flash",
+            contents = request.message,
+            config = {
+            "system_instruction": SYSTEM_INSTRUCTION,
+             "temperature": 0.4,
+             "max_output_tokens":1000
+            }
+        )
+        return {"response":response.text.strip()}
+    except Exception as e:
+        print(f"Gemini Error: {e}")
+
+        raise HTTPException(status_code = 500, detail = "Unable to generate response. ")
